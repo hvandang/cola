adb561a96026037f5098fedb782cd71973e679e4
Hai-Van <hvandang@outlook.com>
2018-01-04 13:54:40 +0000

    new code

--- my_script.py
+++ my_script.py
@@ -1,6 +1,33 @@
-# Sample taken from pyStrich GitHub repository
-# https://github.com/mmulqueen/pyStrich
-id = 0
-while(1):
-	print(id)
-	id = id+1
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', type=str)
+parser.add_argument('password', type=str)
+
+
+class Authenticate(Resource):
+	def post(self):
+		args = parser.parse_args()
+		un = str(args['username'])
+		pw = str(args['password'])
+		return check_auth(un,pw)
+    		
+def check_auth(username, password):
+    return username == 'user' and password == '123'
+    
+api.add_resource(Authenticate, '/auth')
+
+if __name__ == '__main__':
+    app.run(debug=True)
